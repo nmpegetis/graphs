48ceb067fe1868a010ae13082c9f56ce211f61c6
nmpegetis <nmpegetis@gmail.com>
2015-06-12 02:17:39 +0300

    small

--- php/index_code.php
+++ php/index_code.php
@@ -2796,7 +2796,8 @@ else if (/^Full*/.test(experimentName)){
 //					return d.index;
 //				})
 //				.text(function(d){return d.name});
-                grantsElem.unbind().multiselect({
+            grantsElem.multiselect('rebuild')
+            grantsElem.unbind().multiselect({
                     maxHeight: 200,
                     buttonWidth: '200px',
                     buttonContainer: '<div class="btn-group" id="grantsButton"></div>',

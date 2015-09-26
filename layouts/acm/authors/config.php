cd0d826adeed960b88486df2cca3fc271f293f39
nmpegetis <nmpegetis@gmail.com>
2015-09-26 11:04:16 +0300

    authors config file updated

--- layouts/acm/authors/config.php
+++ layouts/acm/authors/config.php
@@ -8,7 +8,7 @@
 $max_execution_time = 120;  //300 seconds = 5 minutes
 $memory_limit = '4096M';	//'-1';		// unlimited memory
 $memcache_port = 11211;
-$db_name = "acmdata1.db";
+$db_name = "PTM3DB.db";
 $db_path = "../../../dbs/".$db_name;
 
 $memcache_time = 2592000;				//600 = 10 minutes 		//2592000 = 30 days (maximum for memcached) //600 = 10 minutes
@@ -29,16 +29,24 @@ $query_topics_nosort = "select TopicId,Item, WeightedCounts from topicdescriptio
 $query_topicsdistribution = null;
 //$query_treemap = "select * from treemapview";
 $query_treemap = null;
-$query_trends = "select * from top50distributionview";
-
+$query_trends = "Select * from TopicDistributionPerBatch
+INNER JOIN TopicDescription on
+TopicDescription.TopicId=TopicDistributionPerBatch.TopicId
+AND TopicDescription.ExperimentId=TopicDistributionPerBatch.ExperimentId
+and  TopicDescription.VisibilityIndex>2
+where JournalISSN IS NULL AND (TopicDistributionPerBatch.TopicId IN
+(15,25,36,38,42,
+44,77,79,100,124,137,141,141,146,149,151,159,168,171,175,178,179,180,186,191,196,204,209,220,223,228,236,255,259,263,264,267,269,274,277,278,279,287,290,294,299,301,321,328,332,335,345,346,349,353,355,356,363,365,368,374,380,382))
+order by topicid,batchid";
 
 ///////////////////////////////////
 ///// front-end configuration /////
 ///////////////////////////////////
 
 $title = "ACM Authors";								// title of the webpage
+$trends_title = "ACM Topics Distribution";								// title of the webpage when ACM
 $subtitle = "";								// subtitle of the webpage
-$experimentName = "ACM_250T_1000IT_0IIT_100B_4M_cos";	// first experiment to load
+$experimentName = "ACM_400T_1000IT_0IIT_100B_5M_cos";	// first experiment to load
 $experimentDescription = "Topic modeling based on: 1)Abstracts from ACM publications 2)Authors 3)Citations 4)ACMCategories SimilarityType:cos Similarity on Authors & Categories"; 	// first description to load
 $node_name = "Author";
 $node_groupName1 = "Authors";
@@ -54,7 +62,7 @@ $fontsizeVar = 26; 				// 43 fontsizeVar variable multiplied with previous_scale
 $smallestFontVar = 13;
 
 // similarities for the graph labeling 
-$expsimilarity = 0.55;			// similarity for querying the database
+$expsimilarity = 0.75;			// similarity for querying the database
 $similarityThr = 0.68;			// similarity threshold
 $nodeConnectionsThr = 0.1; 		// node connections threshold
 $maxNodeConnectionsThr = 0; 	//percentage of minimum node connections for label printing

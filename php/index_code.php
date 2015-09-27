d4247b9d8e7e46df119a918e842d9ee4c17b3b57
nmpegetis <nmpegetis@gmail.com>
2015-09-27 14:39:07 +0300

    force ajaxtrends to execute

--- php/index_code.php
+++ php/index_code.php
@@ -1968,7 +1968,9 @@
 
             function ajaxGraphCall(experiment,expsimilarity) {
 
-                trendsjsonfilename = "../data/trends.json";
+//todo epitides to allaksa to apo katw gia na min to brei kai na ektelestei to ajaxtrends gia na parw ta kainouriga dedomena
+                trendsjsonfilename = "../data/trends1.json";
+//                trendsjsonfilename = "../data/trends.json";
                 trendsPositionsExist=UrlExists(trendsjsonfilename);  //graph positions set true if json file exists
 
                 if (trendsPositionsExist){

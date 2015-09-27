b383939145fbeb5cd3cd98ad447b02f36a792462
nmpegetis <nmpegetis@gmail.com>
2015-09-27 13:07:47 +0300

    permission for writinga

--- layouts/acm/categories/saveGraphPositions.php
+++ layouts/acm/categories/saveGraphPositions.php
@@ -2,7 +2,7 @@
 $myFile = "data/graph_".$_POST["experiment"]."_".$_POST["similarity"]."_".$_POST["gravity"]."_".$_POST["charge"].".json";
 //chmod($myFile, 0777);
 
-$fh = fopen($myFile, 'w') or die("can't open file");
+$fh = fopen(".$myFile.", 'w') or die("can't open file");
 //$stringData = $_POST["data"];
 
 $everything = array();

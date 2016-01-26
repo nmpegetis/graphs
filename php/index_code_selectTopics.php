a2c12679db18130e22b36980edd6d63f19e244a0
nmpegetis <nmpegetis@gmail.com>
2016-01-26 17:07:59 +0200

    Merge branch 'master' of https://github.com/nmpegetis/graphs
    # Conflicts:
    #	php/dbfront_code.php

--- php/index_code_selectTopics.php
+++ php/index_code_selectTopics.php
@@ -59,10 +59,13 @@
     </div>
 
 
+    <script type="text/javascript" src="../../../js/d3.js"></script>
+    <script type="text/javascript" src="../../../js/jquery.js"></script>
+    <script type="text/javascript" src="../../../js/multiselect.min.js"></script>
+    <script type="text/javascript" src="../../../js/bootstrap.js"></script>
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/prettify/r298/prettify.min.js"></script>
-<script type="text/javascript" src="../../../js/multiselect.min.js"></script>
 
 <script>
     var experiment;
